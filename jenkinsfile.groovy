78a4e168d66d52afe8741e7d8cf7474aa6fd37ba
Habeeb <58876091+hballow2008@users.noreply.github.com>
2020-04-20 04:03:13 +0100

    Add files via upload

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -5,7 +5,7 @@ pipeline {
             steps {
                 withAWS(region:'us-east-2',credentials:'AKIA2RYGZ3D3DITFVSNY') {
                     s3Delete(bucket: 'jenkins-bucket5', path:'**/*')
-                    s3Upload(bucket: '<jenkins-bucket5>', file:'index.html');
+                    s3Upload(bucket: 'jenkins-bucket5', file:'index.html');
                     }
                 
               }
